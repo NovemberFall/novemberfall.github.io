376158214f0a619d43354bc22a39f5e4e9e2da54
Git <1219.zheng@gmail.com>
2019-08-14 09:50:34 +0800

    简书

---
+++ book/precache-manifest.962084f26b76005059ab28e80befbdc6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8aa4398b5b036edb6312ae3a0a6ae79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61568d15968c4ec98406",
+    "url": "/static/css/main.6ed0d39e.chunk.css"
+  },
+  {
+    "revision": "6317b025c0f4700fdf88",
+    "url": "/static/js/2.abe94928.chunk.js"
+  },
+  {
+    "revision": "61568d15968c4ec98406",
+    "url": "/static/js/main.93dd00ef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aa3fe53000a4a5d2f89367db78331dc7",
+    "url": "/static/media/iconfont.aa3fe530.woff"
+  },
+  {
+    "revision": "bda4dcf7aeb3cd4477a8848845abda71",
+    "url": "/static/media/iconfont.bda4dcf7.eot"
+  },
+  {
+    "revision": "cde135f3e68d2aad61c81e077265e483",
+    "url": "/static/media/iconfont.cde135f3.svg"
+  },
+  {
+    "revision": "eb2416beb2def3e1b5aabb8de85e57ab",
+    "url": "/static/media/iconfont.eb2416be.ttf"
+  }
+]);
