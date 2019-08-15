b4dc5c88e6531969be660fdb0df0eadd0a619932
Git <1219.zheng@gmail.com>
2019-08-15 11:31:42 +0800

    simple book

---
+++ build/precache-manifest.d846044aef57d068f98af268fb013fd4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ccf6eb408bfe7b3e135645c63886ed",
+    "url": "/book/index.html"
+  },
+  {
+    "revision": "20eb4c20a06d7ad82cd2",
+    "url": "/book/static/css/main.405ba340.chunk.css"
+  },
+  {
+    "revision": "e4be105eaa08e3dd8192",
+    "url": "/book/static/js/2.090f0f9a.chunk.js"
+  },
+  {
+    "revision": "20eb4c20a06d7ad82cd2",
+    "url": "/book/static/js/main.3f038d60.chunk.js"
+  },
+  {
+    "revision": "32ef9d0f082932ab0295",
+    "url": "/book/static/js/runtime~main.938e06c9.js"
+  },
+  {
+    "revision": "aa3fe53000a4a5d2f89367db78331dc7",
+    "url": "/book/static/media/iconfont.aa3fe530.woff"
+  },
+  {
+    "revision": "bda4dcf7aeb3cd4477a8848845abda71",
+    "url": "/book/static/media/iconfont.bda4dcf7.eot"
+  },
+  {
+    "revision": "cde135f3e68d2aad61c81e077265e483",
+    "url": "/book/static/media/iconfont.cde135f3.svg"
+  },
+  {
+    "revision": "eb2416beb2def3e1b5aabb8de85e57ab",
+    "url": "/book/static/media/iconfont.eb2416be.ttf"
+  }
+]);
